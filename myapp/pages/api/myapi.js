e15dd2e5af5f783c140ea7c0624783e09f3e2a13
Abdul Wasay <wasaysaleem071@gmail.com>
2023-11-06 15:59:33 -0800

    hhj

---
+++ myapp/pages/api/myapi.js
@@ -0,0 +1,9 @@
+
+export default function handler(req, res) {
+    if (req.method==="POST"){
+        res.status(200).json({ name: 'John Doe' })
+    }
+    res.status(404).json({ name: 'John Doe' })
+
+  }
+  
